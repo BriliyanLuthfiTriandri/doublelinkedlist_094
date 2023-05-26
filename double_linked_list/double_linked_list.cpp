4c7f219058555aa9c6dceb1a6c42cc16f2a45362
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-05-26 17:54:12 +0700

    menambahkan fungsi doublelinkedlist::traverse

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -102,3 +102,16 @@ bool DoubleLinkedlist::listEmpty() {
 	return (START == NULL);
 }
 
+void DoubleLinkedlist::traverse() {
+	if (listEmpty())
+		cout << "\nlist is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
